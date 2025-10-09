4736d0d708c24f6ea4009a5b0faf316bf02ae901
Mikhail <topseader@gmail.com>
2025-10-09 19:07:56 +0300

    Small refactor

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
-from printer import ConsolePrinter 
+import argparse
+from printer import ConsolePrinter
 from player import HumanPlayer
 
+
 class Game:
-    def __init__(self, player_x, player_o, sym, board_size, printer):
+    def __init__(self, player_x, player_o, sym, board_size: int, printer):
         self.player_x = player_x
         self.player_o = player_o
         self.sym = sym
@@ -14,122 +16,136 @@ class Game:
         else:
             self.win_len = 5
 
-        self.board = [['   ' for _ in range(board_size)]for _ in range(board_size)]
+        self.board = [["   " for _ in range(board_size)] for _ in range(board_size)]
         self.printer = printer
 
-
     def check_move(self, player):
-        while True:            
+        while True:
             try:
                 sym, row, col = player.make_move()
                 self.last_move = sym, row, col
-                self.board[row][col] != '   '
+                self.board[row][col] != "   "
             except UnboundLocalError:
-                print('Invalid input! Row or column number is out of range.')
+                print("Invalid input! Row or column number is out of range.")
                 continue
             except IndexError:
-                print('Invalid input! Row or column number is out of range.')
+                print("Invalid input! Row or column number is out of range.")
                 continue
 
-            if self.board[row][col] != '   ':
-                print('Cell is occupied!')
+            if self.board[row][col] != "   ":
+                print("Cell is occupied!")
                 continue
             else:
                 break
 
         return True
 
-
     def get_move(self, player):
         if self.check_move(player):
             sym, row, col = self.last_move
-        
-        self.board[row][col] = sym
-
+            self.board[row][col] = sym
 
     def is_win(self):
         sym, row, col = self.last_move
-        
-        #rows
-        for col_num in range(self.board_size - self.win_len):
-            if all(self.board[row][col_num + i] == sym 
-                    for i in range(self.win_len)):
-                        return True
 
-        #columns
-        for row_num in range(self.board_size - self.win_len):
-            if all(self.board[row_num + i][col] == sym 
-                    for i in range(self.win_len)):
+        # rows
+        for col_num in range(self.board_size - self.win_len):
+            if all(self.board[row][col_num + i] == sym for i in range(self.win_len)):
                 return True
 
-        #diagonals
+        # columns
+        for row_num in range(self.board_size - self.win_len):
+            if all(self.board[row_num + i][col] == sym for i in range(self.win_len)):
+                return True
+
+        # diagonals
         offset = 0
 
-        #top-left to bottom-right
-        #above the middle
+        # top-left to bottom-right
+        # above the middle
         if row <= col:
             while col - row + self.win_len + offset - 1 <= self.board_size:
-                if all(self.board[i+offset][col-row+i+offset] == sym 
-                    for i in range(self.win_len)):
-                        return True
+                if all(
+                    self.board[i + offset][col - row + i + offset] == sym
+                    for i in range(self.win_len)
+                ):
+                    return True
                 offset += 1
-        #below the middle
+
+        # below the middle
         else:
             while row - col + self.win_len + offset - 1 <= self.board_size:
-                if all(self.board[row-col+i+offset][i+offset] == sym 
-                    for i in range(self.win_len)):
-                        return True
+                if all(
+                    self.board[row - col + i + offset][i + offset] == sym
+                    for i in range(self.win_len)
+                ):
+                    return True
                 offset += 1
 
-        #top-right to bottom-left
+        # top-right to bottom-left
         offset = 0
 
-        #above the middle
+        # above the middle
         if row + col <= self.board_size - 1:
             while row + col - offset >= self.win_len - 1:
-                if all(self.board[i+offset][row+col-i-offset] == sym 
-                    for i in range(self.win_len)):
-                        return True
+                if all(
+                    self.board[i + offset][row + col - i - offset] == sym
+                    for i in range(self.win_len)
+                ):
+                    return True
                 offset += 1
-        #below the middle
-        else:            
-            while row + col + offset < self.board_size*2 - self.win_len:
-                if all(self.board[row+col+1-self.board_size+i+offset]
-                    [self.board_size-1-i-offset] == sym 
-                    for i in range(self.win_len)):
-                        return True
+        # below the middle
+        else:
+            while row + col + offset < self.board_size * 2 - self.win_len:
+                if all(
+                    self.board[row + col + 1 - self.board_size + i + offset][
+                        self.board_size - 1 - i - offset
+                    ]
+                    == sym
+                    for i in range(self.win_len)
+                ):
+                    return True
                 offset += 1
-        
-        
+        # if nothing matches
         return False
-
 
     def play(self):
         self.printer.help()
-        for n_turns in range(self.board_size ** 2):
+        for _ in range(self.board_size**2):
             self.get_move(self.player_x)
             self.printer.print_board(self.board)
             if self.is_win():
-                print('X player is win')
+                print("X player is win")
                 break
 
-            print('O player\'s turn')
+            print("O player's turn")
             self.get_move(self.player_o)
             self.printer.print_board(self.board)
             if self.is_win():
-                print('O player is win')
+                print("O player is win")
                 break
-            print('X player\'s turn')
+            print("X player's turn")
 
 
 def main():
-    BOARD_SIZE = 13
-    pl_x = HumanPlayer(' X ')
-    pl_o = HumanPlayer(' O ')
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s",
+        "--size",
+        type=int,
+        default=13,
+        nargs="?",
+        help="Start date of the report in 'YYYY-MM-DD' format. Defaults to start of previous week",
+    )
+    args = parser.parse_args()
+
+    BOARD_SIZE = args.size
+    pl_x = HumanPlayer(" X ")
+    pl_o = HumanPlayer(" O ")
     console_print = ConsolePrinter(BOARD_SIZE)
-    game = Game(pl_x, pl_o, ' X ', BOARD_SIZE, console_print)
+    game = Game(pl_x, pl_o, " X ", BOARD_SIZE, console_print)
     game.play()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,37 +1,30 @@
-#from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
+
 
 class HumanPlayer:
     def __init__(self, sym):
         self.sym = sym
         self.ai = False
 
-
     def make_move(self):
         raw_str = input()
-        if raw_str[0] in list('0123456789'):
-            print('Invalid input! Use letters to determine targeted row.')
+        if raw_str[0] in list("0123456789"):
+            print("Invalid input! Use letters to determine targeted row.")
             self.make_move()
         try:
             row = self.correct_row(raw_str[0])
             col = int(raw_str[1:3])
         except ValueError:
-            print('Invalid input!')
+            print("Invalid input!")
             self.make_move()
 
         return (self.sym, row, col)
 
-
     @staticmethod
-    def correct_row(char):      # "B3" => row=2; col=3
+    def correct_row(char):  # "B3" => row=2; col=3
         row = ord(char.upper())
-        row -= 65               # 'a' => 0
+        row -= 65  # 'a' => 0
         return row
-
-
-
-
-
-
 
 
 class AIPlayer:
